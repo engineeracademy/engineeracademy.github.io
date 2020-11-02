ef4cbed35dee805ee3e1c50dca9bd7a6733ee6dd
Talha Mirza <talha.mirza.99@gmail.com>
2020-11-03 00:50:50 +0500

    New: Categories | pg-to-8.html added

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+console.log("JavaScript will Show the Number Link On Mobile Devices")
+console.log("These Devices will be checked:")
+console.log("Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini")
+console.log("Thus it will be Hidden on Desktop so No Error on clicking the number incase there is no call app such as skype installed")
 
 var normalVer = document.getElementsByClassName("normal-ver")
 var mobileVer = document.getElementsByClassName("mobile-ver")
@@ -12,3 +16,5 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
         mobileVer[0].style.display = "none";
     	console.log("DETECTED: DESKTOP")
 }
+
+// alert("Click On the Number to Whatsapp")
